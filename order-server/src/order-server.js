828c53344adab8ca37abeedef5abb4db0053b1bc
naser tayeh <nasertayeh82@gmail.com>
2023-11-14 13:26:11 +0200

    edit order server to solve purshase issue

--- order-server/src/order-server.js
+++ order-server/src/order-server.js
@@ -15,13 +15,13 @@ const catalogServerOptions = {
 
 //in order server just purchase api
 // the api called by frontend server and then send response to frontend
-// http://172.18.0.8:8002/purchase/subject
-app.post("/purchase/:subject", (req, res) => {
-  const { subject } = req.params; //get item id obj or item name
+// http://172.18.0.8:8002/purchase/itemID
+app.post("/purchase/:itemID", (req, res) => {
+  const { itemID } = req.params; //get item id obj or item name
 
   //call catalog server to chick if the item is in stock
   const catalogRequest = http.get(
-    `http://${catalogServerOptions.hostname}:${catalogServerOptions.port}/purchase/${subject}`,
+    `http://${catalogServerOptions.hostname}:${catalogServerOptions.port}/purchase/${itemID}`,
     //response from server catalog
     (catalogResponse) => {
       let data = "";
@@ -34,16 +34,21 @@ app.post("/purchase/:subject", (req, res) => {
       catalogResponse.on("end", () => {
         const catalogData = JSON.parse(data); //convert from String to json
 
+        //if item not found
+        if (catalogResponse.statusCode == 404) {
+          return res.status(404).json({ message: catalogData.message }); //item not found
+        }
+
         // Check if the item is in stock
         if (!catalogData || catalogData.quantity <= 0) {
-          return res.status(400).json({ error: "Item out of stock" });
+          return res.status(400).json({ message: "Item out of stock" }); // "Item out of stock"
         }
         // if found in stock send PUT query to catalog server to update on item quantity decrement by one
-        //call catalog service http://172.18.0.8:8002/update/subject
+        //call catalog service http://172.18.0.8:8002/update/itemID
         const decrementRequestOptions = {
           hostname: catalogServerOptions.hostname,
           port: catalogServerOptions.port,
-          path: `/update/${subject}`,
+          path: `/update/${itemID}`,
           method: "PUT",
         };
         const decrementRequest = http.request(
@@ -59,20 +64,28 @@ app.post("/purchase/:subject", (req, res) => {
 
             decrementResponse.on("end", () => {
               const responseText = responseData.join("");
-              if (decrementResponse.statusCode === 200) {
-                const responseObject = JSON.parse(responseText); //convert response to json
-                res.json({
-                  message: `Item ${responseObject.title} purchased successfully`, // title here is item name
-                });
+              const responseObject = JSON.parse(responseText); //convert response to json
+
+              if (decrementResponse.statusCode == 404) {
+                //item not found
+                res.status(404).json({ message: responseObject.message });
+              } else if (decrementResponse.statusCode == 400) {
+                //item out of stock
+                res.status(400).json({ message: responseObject.message });
               } else {
-                res.status(500).json({
-                  error: "Error updating stock",
-                  response: responseData,
+                res.status(200).json({
+                  message: `Bought book \'${responseObject.title}\' successfully`, // title here is item name
                 });
               }
             });
           }
         );
+        decrementRequest.on("error", (error) => {
+          //error in catalog server update
+          res
+            .status(500)
+            .json({ error: "Error Update item quantity: " + error.message });
+        });
         decrementRequest.end();
       });
     }
